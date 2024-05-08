6b2568fdf94d2df5b9329735ecf81acaa99eec61
arvirabago <rabago.arvilyzadessa@gmail.com>
2024-05-08 20:08:41 +0800

    Homepage fixed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <HelmetProvider>
       <Helmet>
-        <title>Juan Dela Cruz</title>
+        <title>Arvilyza Dessa Rabago</title>
       </Helmet>
 
       {/* Home Component */}
